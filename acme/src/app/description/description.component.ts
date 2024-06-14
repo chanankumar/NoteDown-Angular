b2fb6f46f62961a59357f394201b1e8a87ccb86a
chanankumar <chanankumar.ck@gmail.com>
2024-06-14 19:43:22 +0530

    description component feature resusable component

--- acme/src/app/description/description.component.ts
+++ acme/src/app/description/description.component.ts
@@ -7,5 +7,23 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class DescriptionComponent {
 
+  @ViewChild('autoResizeTextarea') textarea!: ElementRef<HTMLTextAreaElement>;
+
+
+  ngAfterViewInit(): void {
+    this.adjust();
+  }
+
+  onInput(): void {
+    this.adjust();
+  }
+
+  private adjust(): void {
+    const textarea = this.textarea.nativeElement;
+    textarea.style.overflow = 'hidden';
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  } 
+  
 }
 
